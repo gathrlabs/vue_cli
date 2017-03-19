6919af483b3952eaed5ad8376094c63a36df1dad
Gathr Labs <team@gathrlabs.com.au>
2017-03-19 17:14:36 +1000

    Start of module

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,34 +1,19 @@
 <template>
-    <div class="page">
-        <app-header></app-header>
-          <div class="section">
-            <div class="container">
-              <div class="columns">
-                <servers></servers>
-                <app-server-details></app-server-details>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               
             </div>
-          </div>
         </div>
-        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
-    import Header from './components/Shared/Header.vue';
-    import Footer from './components/Shared/Footer.vue';
-    import Servers from './components/Server/Servers.vue';
-    import ServerDetails from './components/Server/ServerDetails.vue';
 
     export default {
-        components: {
-            appHeader: Header,
-            Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer
-        }
+        
     }
 </script>
 
-<style lang="sass">
-  @import "~bulma";
+<style>
 </style>
--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,9 +1,5 @@
-import Vue  from 'vue'
-import App  from './App.vue'
-
-export const serverBus = new Vue();
-
-//This is the event bus that we have created to allow the siblings [ServerDetails and Server] to communicate with one another. Where Server is $emit the event and ServerDetails is listening for the event. 
+import Vue from 'vue'
+import App from './App.vue'
 
 new Vue({
   el: '#app',
