bb7e8930edb3b0275a551c348880589d140010b8
Gathr Labs <team@gathrlabs.com.au>
2017-03-19 11:59:41 +1000

    start of assignment

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,22 +1,34 @@
 <template>
-  <div>
-    <my-header></my-header>
-    <my-server-list></my-server-list>
-    <my-footer></my-footer>
-  </div>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+          <div class="section">
+            <div class="container">
+              <div class="columns">
+                <servers></servers>
+              <app-server-details></app-server-details>
+            </div>
+          </div>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
-  import Header from './Header.vue'
-  import Servers from './SeverList.vue'
-  import Footer from './Footer.vue'
-  export default {
-    components: {
-      'my-header': Header,
-      'my-server-list': Servers,
-      'my-footer': Footer
+    import Header from './components/Shared/Header.vue';
+    import Footer from './components/Shared/Footer.vue';
+    import Servers from './components/Server/Servers.vue';
+    import ServerDetails from './components/Server/ServerDetails.vue';
+
+    export default {
+        components: {
+            appHeader: Header,
+            Servers,
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
+        }
     }
-  }
 </script>
 
 <style lang="sass">
