3090446ca8cdcbeff0ee78f1ba807bcaa4189b72
Gathr Labs <team@gathrlabs.com.au>
2017-03-19 16:35:55 +1000

    Fully completed asssignment with notes

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -3,6 +3,8 @@ import App  from './App.vue'
 
 export const serverBus = new Vue();
 
+//This is the event bus that we have created to allow the siblings [ServerDetails and Server] to communicate with one another. Where Server is $emit the event and ServerDetails is listening for the event. 
+
 new Vue({
   el: '#app',
   render: h => h(App)
