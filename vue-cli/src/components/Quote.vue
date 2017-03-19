56fa4f25d6db0e1bf8a885782c8e3fc487835046
Gathr Labs <team@gathrlabs.com.au>
2017-03-19 20:27:06 +1000

    start of assignment

---
+++ vue-cli/src/components/Quote.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      <slot name="title"></slot>
+    </div>
+    <div>
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<styles scoped>
+
+</styles>
